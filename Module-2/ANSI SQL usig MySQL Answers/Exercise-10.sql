d5dcd1dca49f2448acb23e7127d016c05e0a7ace
Bindu Miryala <bindumiryala2605@gmail.com>
2025-05-27 14:19:24 +0530

    Delete Module-2/ANSI SQL usig MySQL Answers directory

--- Module-2/ANSI SQL usig MySQL Answers/Exercise-10.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT e.title
-FROM Events e
-JOIN Registrations r ON e.event_id = r.event_id
-WHERE e.event_id NOT IN (
-    SELECT DISTINCT event_id FROM Feedback
-);
--- Module-2/ANSI SQL usig MySQL Answers/Exercise-16.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM Users
-WHERE registration_date >= CURDATE() - INTERVAL 30 DAY
-AND user_id NOT IN (
-    SELECT DISTINCT user_id FROM Registrations
-);
